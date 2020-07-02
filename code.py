f51442a399d05853da2c0192d4b4f33643600d72
winterzakwolf <65005095+winterzakwolf@users.noreply.github.com>
2020-07-02 19:42:26 +0200

    Create code.py

---
+++ code.py
@@ -0,0 +1,58 @@
+#grasp the audio (it is pretty easier to grasp audio from a mic).
+# the file name output you want to record into
+filename = "recorded.wav"
+# set the chunk size of 1024 samples
+chunk = 1024
+# sample format
+FORMAT = pyaudio.paInt16
+# mono, change to 2 if you want stereo
+channels = 1
+# 44100 samples per second
+sample_rate = 44100
+record_seconds = 10
+# initialize PyAudio object
+p = pyaudio.PyAudio()
+# open stream object as input & output
+stream = p.open(format = FORMAT,
+                channels = channels,
+                rate = sample_rate,
+                input = True,
+                input_device_index = dev_index,
+                frames_per_buffer = chunk)
+frames = []
+print("Recording...")
+for i in range(int(44100 / chunk * record_seconds)):
+    data = stream.read(chunk)
+    # if you want to hear your voice while recording
+    # stream.write(data)
+    frames.append(data)
+print("Finished recording.")
+# stop and close stream
+stream.stop_stream()
+stream.close()
+# terminate pyaudio object
+p.terminate()
+# save audio file
+# open the file in 'write bytes' mode
+wf = wave.open(filename, "wb")
+# set the channels
+wf.setnchannels(channels)
+# set the sample format
+wf.setsampwidth(p.get_sample_size(FORMAT))
+# set the sample rate
+wf.setframerate(sample_rate)
+# write the frames as bytes
+wf.writeframes(b"".join(frames))
+# close the file
+wf.close()
+
+#speech to text
+import speech_recognition as sr
+
+r=sr.Recognizer()
+
+sound=sr.AudioFile('recorded.wav')
+with sound as source:
+    audio=r.record(source)
+val=r.recognize_google(audio)
+print(val)
